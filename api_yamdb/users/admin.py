3c17d8eb0f4eca2727b8fb7bde8d43ae817c4fab
Ellipsis99 <suharev870@yandex.ru>
2026-06-05 03:28:15 +0300

    Revert "feature/Remizov-users"

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,33 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from users.models import User
-
-
-@admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    list_display = (
-        'username', 'email', 'first_name',
-        'last_name', 'role', 'is_active'
-    )
-    list_filter = ('role', 'is_active', 'is_staff')
-    fieldsets = (
-        (None, {'fields': ('username', 'email', 'password')}),
-        ('Персональные данные', {
-            'fields': ('first_name', 'last_name', 'bio')
-        }),
-        ('Права доступа', {
-            'fields': ('role', 'is_active', 'is_staff', 'is_superuser')
-        }),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': (
-                'username', 'email', 'password1',
-                'password2', 'role'
-            ),
-        }),
-    )
-    search_fields = ('username', 'email')
-    ordering = ('username',)
+# Register your models here.
